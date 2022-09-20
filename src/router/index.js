0b56edf0eb2ec71493dd598d625ba18c297839b9
noieeen <tharnnn1401@gmail.com>
2022-09-21 02:32:17 +0700

    view product detail

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+import Product from '../views/Product.vue'
+
 const routes = [
   {
     path: '/',
@@ -14,6 +16,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path:'/:category_slug/:product_slug/',
+    name:'Product',
+    component: Product
   }
 ]
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,73 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <section class="hero is-medium is-dark mb-6">
+      <div class="hero-body has-text-centered">
+        <p class="title mb-6">
+          Welcome to the Djackets
+        </p>
+        <p class="subtitle">
+          The best jacket store online
+        </p>
+      </div>
+    </section>
+
+    <div class="columns is-multiline">
+      <div class="column is-12">
+        <h2 class="is-size-2 has-text-centered">Lastest Products</h2>
+      </div>
+
+      <div class="column is-3" v-for="product in latestProducts" v-bind:key="product.id">
+        <div class="box">
+          <figure class="image mb-4">
+            <img :src="product.get_thumbnail">
+          </figure>
+
+          <h3 class="is-size-4">{{product.name}}</h3>
+          <p class="is-size-6 has-text-grey">${{product.price}}</p>
+
+          <router-link v-bind:to="product.get_absolute_url" class="button is-dark mt-4">Detail</router-link>
+        </div>
+ 
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import axios from 'axios'
 
 export default {
   name: 'HomeView',
+  data(){
+    return{
+      latestProducts:[]
+    }
+  },
   components: {
-    HelloWorld
+    
+  },
+  mounted(){
+    this.getLastestProducts()
+  },
+  methods:{
+    getLastestProducts(){
+      axios.get('/api/v1/latest-products/')
+      .then(response =>{
+        this.latestProducts = response.data
+        console.log(this.latestProducts)
+      }).catch(error =>{
+        console.error(error)
+      })
+    }
   }
 }
 </script>
+
+<style scoped>
+.image{
+  margin-top:-1.25rem;
+  margin-left: -1.25rem;
+  margin-right:-1.25rem;
+}
+</style>
