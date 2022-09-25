52589d1981f44d32bfa78329294f4da1e1a22afa
noieeen <tharnnn1401@gmail.com>
2022-09-26 05:19:44 +0700

    400 Bad

--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -66,7 +66,12 @@
             <div class="field">
               <label for="">Phone*</label>
               <div class="control">
-                <input type="number" class="input" maxlength="10" v-model="phone" />
+                <input
+                  type="number"
+                  class="input"
+                  maxlength="10"
+                  v-model="phone"
+                />
               </div>
             </div>
           </div>
@@ -101,15 +106,14 @@
         <hr />
 
         <div id="card-element" class="column is-6 mb-5"></div>
-            <!-- Used to display Element errors. -->
+        <!-- Used to display Element errors. -->
         <div id="card-errors" role="alert"></div>
-          <template v-if="cartTotalLength">
-            <hr />
-            <button class="button is-dark" @click="submitForm">
-              Pay with Stripe
-            </button>
-          </template>
-        
+        <template v-if="cartTotalLength">
+          <hr />
+          <button class="button is-dark" @click="submitForm">
+            Pay with Stripe
+          </button>
+        </template>
       </div>
     </div>
   </div>
@@ -136,18 +140,52 @@ export default {
       zipcode: "",
       place: "",
       errors: [],
+      stripe_pk:''
     };
   },
-  mounted() {
+  async mounted() {
     document.title = "Checkout | Django Store";
 
+    this.stripe_pk = "pk_test_51LlyLZFOGO7J1192Okwoe4f8ihvtcmm9cidVqRRK7mMIfudQJOdRou2vjRLRZDO53vkha17eJ5hqbXXqacWH5QfI005sBg17NI"
     this.cart = this.$store.state.cart;
 
-    if(this.cartTotalLength>0){
-        this.stripe = Stripe('pk_test_51LlyLZFOGO7J1192Okwoe4f8ihvtcmm9cidVqRRK7mMIfudQJOdRou2vjRLRZDO53vkha17eJ5hqbXXqacWH5QfI005sBg17NI')
-        const  elements = this.stripe.elements();
-        this.card = elements.create('card',{hidePostalCode:true});
-        this.card.mount('#card-element')
+
+    // const items = [];
+    //   for (let i = 0; i < this.cart.items.length; i++) {
+    //     const item = this.cart.items[i];
+    //     const obj = {
+    //       product: item.product.id,
+    //       quantity: item.quantity,
+    //       price: item.product.price * item.quantity,
+    //     };
+    //     items.push(obj);
+    //   }
+    //   const data = {
+    //     first_name: this.first_name,
+    //     last_name: this.last_name,
+    //     email: this.email,
+    //     address: this.address,
+    //     zipcode: this.zipcode,
+    //     place: this.place,
+    //     phone: this.phone,
+    //     items: items,
+    //   };
+    //   console.log("data>>", data);
+    if (this.cartTotalLength > 0) {
+      this.stripe = Stripe(this.stripe_pk);
+
+    //   await axios
+    //     .post("/api/v1/checkout/", data)
+    //     .then((response) => {
+    //       console.log("intialize", response.data);
+    //     })
+    //     .catch((error) => {
+    //       console.log(error);
+    //     });
+
+      const elements = this.stripe.elements();
+      this.card = elements.create("card", { hidePostalCode: true });
+      this.card.mount("#card-element");
     }
   },
   methods: {
@@ -157,88 +195,85 @@ export default {
     submitForm() {
       this.errors = [];
 
-      if (this.first_name === "") {
-        this.errors.push("The first name is missing!");
-      }
+    //   if (this.first_name === "") {
+    //     this.errors.push("The first name is missing!");
+    //   }
 
-      if (this.last_name === "") {
-        this.errors.push("The last name is missing!");
-      }
+    //   if (this.last_name === "") {
+    //     this.errors.push("The last name is missing!");
+    //   }
 
-      if (this.email === "") {
-        this.errors.push("The email is missing!");
-      }
+    //   if (this.email === "") {
+    //     this.errors.push("The email is missing!");
+    //   }
 
-      if (this.phone === "") {
-        this.errors.push("The phone is missing!");
-      }
+    //   if (this.phone === "") {
+    //     this.errors.push("The phone is missing!");
+    //   }
 
-      if (this.address === "") {
-        this.errors.push("The address is missing!");
-      }
+    //   if (this.address === "") {
+    //     this.errors.push("The address is missing!");
+    //   }
 
-      if (this.zipcode === "") {
-        this.errors.push("The zipcode is missing!");
-      }
+    //   if (this.zipcode === "") {
+    //     this.errors.push("The zipcode is missing!");
+    //   }
 
-      if (this.place === "") {
-        this.errors.push("The place is missing!");
-      }
+    //   if (this.place === "") {
+    //     this.errors.push("The place is missing!");
+    //   }
 
-      if(!this.errors.length){
-        this.$store.commit('setIsLoading',true);
+      if (!this.errors.length) {
+        this.$store.commit("setIsLoading", true);
 
-        this.stripe.createToken(this.card).then(result =>{
-            if(result.error){
-                this.$store.commit('setIsLoading',false);
-
-                this.errors.push('Something went wrong with Stripe. Please try again.')
-                console.log(result.error.message)
-            }else{
-                this.stripeTokenHandler(result.token)
-                console.log(result.token)
+        this.stripe.createToken(this.card).then(result => {                    
+                    if (result.error) {
+                        this.$store.commit('setIsLoading', false)
+                        this.errors.push('Something went wrong with Stripe. Please try again')
+                        console.log(result.error.message)
+                    } else {
+              this.stripeTokenHandler(result.token);
+              console.log(result.token);
             }
-        })
-
-        
+          });
       }
     },
-    async stripeTokenHandler(token) {
-            const items = []
-            for (let i = 0; i < this.cart.items.length; i++) {
-                const item = this.cart.items[i]
-                const obj = {
-                    product: item.product.id,
-                    quantity: item.quantity,
-                    price: item.product.price * item.quantity
-                }
-                items.push(obj)
-            }
-            const data = {
-                'first_name': this.first_name,
-                'last_name': this.last_name,
-                'email': this.email,
-                'address': this.address,
-                'zipcode': this.zipcode,
-                'place': this.place,
-                'phone': this.phone,
-                'items': items,
-                'stripe_token': token.id
-            }
 
-            console.log('data>>',data)
-            await axios
-                .post('/api/v1/checkout/', data)
-                .then(response => {
-                    this.$store.commit('clearCart')
-                    this.$router.push('/cart/success')
-                })
-                .catch(error => {
-                    this.errors.push('Something went wrong. Please try again')
-                    console.log(error.response)
-                })
-                this.$store.commit('setIsLoading', false)
-        }
+    async stripeTokenHandler() {
+      const items = [];
+      for (let i = 0; i < this.cart.items.length; i++) {
+        const item = this.cart.items[i];
+        const obj = {
+          product: item.product.id,
+          quantity: item.quantity,
+          price: item.product.price * item.quantity,
+        };
+        items.push(obj);
+      }
+      const data = {
+        first_name: this.first_name,
+        last_name: this.last_name,
+        email: this.email,
+        address: this.address,
+        zipcode: this.zipcode,
+        place: this.place,
+        phone: this.phone,
+        items: items,
+      };
+
+      
+      await axios
+        .post("/api/v1/checkout/", data)
+        .then((response) => {
+          this.$store.commit("clearCart");
+          this.$router.push("/cart/success");
+        })
+        .catch((error) => {
+          this.errors.push("Something went wrong. Please try again");
+          console.log(error.response);
+        });
+      this.$store.commit("setIsLoading", false);
+    },
   },
   computed: {
     cartTotalPrice() {
@@ -247,10 +282,10 @@ export default {
       }, 0);
     },
     cartTotalLength() {
-            return this.cart.items.reduce((acc, curVal) => {
-                return acc += curVal.quantity
-            }, 0)
-        }
+      return this.cart.items.reduce((acc, curVal) => {
+        return (acc += curVal.quantity);
+      }, 0);
+    },
   },
 };
 </script>
